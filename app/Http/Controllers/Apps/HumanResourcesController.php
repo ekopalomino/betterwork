06e40230ac614b80e46d8419e97fe128b1ab10db
Eko Heru <heru.palomino@gmail.com>
2021-03-13 22:16:34 +0700

    update

--- app/Http/Controllers/Apps/HumanResourcesController.php
+++ app/Http/Controllers/Apps/HumanResourcesController.php
@@ -1525,7 +1525,7 @@ class HumanResourcesController extends Controller
 
     public function reimbursIndex()
     {
-        $data = EmployeeReimbursment::orderBy('created_at','DESC')->get();
+        $data = EmployeeReimbursment::where('employee_id',auth()->user()->employee_id)->orderBy('created_at','DESC')->get();
 
         return view('apps.pages.reimbursIndex',compact('data'));
     }
